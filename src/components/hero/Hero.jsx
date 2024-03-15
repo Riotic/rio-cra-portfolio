d863ebaefea1afd2fddafc560b2f3170dadf91ef
rio_clement <clemen_r@etna-alternance.net>
2024-03-15 15:50:06 +0100

    ajout parallax

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,22 +1,83 @@
-import "./hero.scss"
+import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType:"mirror",
+      duration: 20,
+    },
+  },
+};
 
 const Hero = () => {
-    return (
-        <div className="hero">
-            
-            <h2>RIO CLEMENT</h2>
-            <h1>Développeur Web fullstack</h1>
-            <div className="buttons">
-                <button>Voir mes derniers projets</button>
-                <button>Contactez moi</button>
-            </div>
-            <img src="/scroll.png" alt=""/>
-            <div className="imageContainer">
-                <img src="/hero2.png" alt=""/>
-            </div>
-        </div>
-        
-    )
-}
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>RIO CLEMENT</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Développeur Web en Alternance
+          </motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              Voir mes derniers projets
+            </motion.button>
+            <motion.button variants={textVariants}>Contactez Moi</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Web Developper Alternant
+      </motion.div>
+      <div className="imageContainer">
+        <img src="/hero3.png" alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,37 @@
-import Sidebar  from '../sidebar/Sidebar';
-import './navbar.scss'
-import {motion} from 'framer-motion';
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-    return (
-    <div className='navbar'>
-        <Sidebar/>
-        {/* <div className="left"*/}
-        <div className='wrapper'>
-            <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration: 0.5}}><a href='#'><img src='/etna.jpg'/></a></ motion.span>
-            {/* put etna img here */}
-            <div className='social'>
-                <a href="#"><img src='/facebook.png'/></a>
-                <a href="#"><img src='/instagram.png'/></a>
-                <a href="#"><img src='/youtube.png'/></a>
-                <a href="#"><img src='/dribbble.png'/></a>
-            </div>
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <img src="/etna.jpg" alt="" />
+        </motion.span>
+        <div className="social">
+          <a href="https://www.linkedin.com/in/rio-clement-57b29a20a">
+            <img src="/linkedin.png" alt="" />
+          </a>
+          <a href="https://www.instagram.com/rio_is_drawing_a_bit/">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="https://www.youtube.com/channel/UCbG95LGkP2VBfnj8f7qWzGA">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="https://github.com/Riotic">
+            <img src="/githublogo.png" alt="" />
+          </a>
         </div>
+      </div>
     </div>
-
-
-    )
-}
+  );
+};
 
 export default Navbar;
